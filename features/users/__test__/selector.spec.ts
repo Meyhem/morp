1681b8ef7769e1df34adf6a05ce59ffe32f1375a
meyhem <meyhem@protonmail.com>
2020-07-29 19:40:26 +0200

    Fix typing

--- features/users/__test__/selector.spec.ts
+++ features/users/__test__/selector.spec.ts
@@ -3,6 +3,7 @@ import { selectUserList } from '../selectors'
 describe('exampleSelectors', () => {
   const state = {
     users: {
+      loading: false,
       users: { '1': { id: '1', name: '123', surname: '432' } },
     },
   }
